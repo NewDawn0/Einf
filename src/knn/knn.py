fc2a0672e5666fca24fa961fd56efdafffb9d9db
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-09-05 23:50:40 +0200

    modify knn

--- src/knn/knn.py
+++ src/knn/knn.py
@@ -3,13 +3,11 @@ from sklearn import neighbors as nb
 
 col_names = ["weight", "height", "size"]
 dataset = pd.read_csv("t-shirts.csv", names=col_names)
-
-x = dataset[["weight", "height"]]
+X = dataset[["weight", "height"]]
 y = dataset[["size"]]
 
-classifier = nb.KNeighborClassifier(n_neighbors=3)
-classifier.fit(x, y)
-avg = x.mean()
+clasifier = nb.KNeighborsClassifier(n_neighbors=3).fit(X, y)
+avg = X.mean()
 x_star = pd.DataFrame(avg).T
-p = classifier.predict(x_star)
+p = clasifier.predict(x_star)
 print(p)
